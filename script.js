dc7952894e8c32028169b197cb50d0fff58ea06c
kerichemman <jeromejkle@gmail.com>
2024-09-10 22:57:58 +0300

    Q Final Commits

--- script.js
+++ script.js
@@ -20,3 +20,66 @@ window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     header.classList.toggle("scrolled", window.scrollY > 50);
 });
+
+const mission = document.querySelector('.mission');
+const values = document.querySelector('.values');
+
+    mission.addEventListener('mouseover', () => {
+    mission.style.backgroundColor = '#e0f7fa';
+    });
+
+    values.addEventListener('mouseover', () => {
+    values.style.backgroundColor = '#e0f7fa';
+    });
+
+    mission.addEventListener('mouseleave', () => {
+    mission.style.backgroundColor = '#fff';
+    });
+
+    values.addEventListener('mouseleave', () => {
+    values.style.backgroundColor = '#fff';
+    });
+
+//testimonials
+
+$(document).ready(function(){
+  $('.testimonial-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+
+
+
+
+function toggleFaq(faq) {
+    const isActive = faq.parentElement.classList.contains('active');
+    document.querySelectorAll('.faq').forEach(item => item.classList.remove('active'));
+  
+    if (!isActive) {
+      faq.parentElement.classList.add('active');
+    }
+  }
+  
+  
